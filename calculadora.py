b7e198866f2437d525b1d15f7ff958a80c2ee971
leticiacaldas <leticia.lbc@hotmail.com>
2024-06-19 16:48:32 -0300

    Adicao e Subtracao

--- calculadora.py
+++ calculadora.py
@@ -1,18 +1,22 @@
 class Calculadora:
     def adicionar(self, a, b):
         # TODO: Implemente a adição
+        return a + b
         pass
 
     def subtrair(self, a, b):
         # TODO: Implemente a subtração
+        return a - b
         pass
 
     def multiplicar(self, a, b):
-        # TODO: Implemente a multiplicação
+        #TODO: Implemente a multiplicação
+        return a * b
         pass
 
     def dividir(self, a, b):
         # TODO: Implemente a divisão
         # Certifique-se de lidar com a divisão por zero
+        return a / b
         pass
 
